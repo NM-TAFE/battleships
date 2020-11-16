afefcb6cd1fb12035ed359d16eb6f6d2b6f4b47e
Dylan Valverde <20047221@tafe.wa.edu.au>
2020-11-16 17:17:44 +0800

    School Commit

--- clients/20047221/app/main.py
+++ clients/20047221/app/main.py
@@ -36,6 +36,17 @@ enemyGrid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 global current_x
 global current_y
 
+A = input('Where do you want to place the Aircraft Carrier? : ')
+for Achar in A:
+    if Achar.isdigit():
+        x = int(Achar) - 1
+    elif Achar.isupper():
+        y = ord(Achar) - 65
+    elif Achar.islower():
+        y = ord(Achar) - 97
+    else:
+        print('Invalid Character')
+
 
 @battleship.on()
 def begin():
@@ -49,7 +60,7 @@ def start_turn():
     for char in s:
         if char.isdigit():
             global current_x
-            current_x = char - 1
+            current_x = int(char) - 1
         elif char.isupper():
             global current_y
             current_y = ord(char) - 65
@@ -65,19 +76,19 @@ def start_turn():
 @battleship.on()
 def hit():
     print('You hit the target!')
-    playerGrid[current_y][current_x] = 'H'
+    playerGrid[current_y][current_x] = 'X'
 
 
 @battleship.on()
 def miss():
     print('Aww.. You missed!')
-    enemyGrid[current_y][current_x] = 'M'
+    enemyGrid[current_y][current_x] = 'O'
 
 
 @battleship.on()
 def win():
     print('Yay! You won!')
-    playerGrid[current_y][current_x] = 'H'
+    playerGrid[current_y][current_x] = 'X'
     playing.clear()
 
 
@@ -91,31 +102,32 @@ def lose():
 def attack(vector):
     vector = vector[0]
     print(f'Shot received at {vector}')
-
+    enemy_x = 0
+    enemy_y = 0
     for char in vector:
         if char.isdigit():
-            x = char - 1
+            enemy_x = char - 1
         elif char.isupper():
-            y = ord(char) - 65
+            enemy_y = ord(char) - 65
         elif char.islower():
-            y = ord(char) - 97
+            enemy_y = ord(char) - 97
         else:
             print('Invalid Character')
 
     while True:
-        print("""H)it, m)iss, or d)efeat?""")
+        print("""H)it, M)iss, or D)efeat?""")
         s = input('Enter status> ')
         _s = s[0].upper()
         if _s == 'H':
-            playerGrid[y][x] = 'H'
+            playerGrid[enemy_y][enemy_x] = 'X'
             battleship.hit()
             break
         elif _s == 'M':
-            playerGrid[y][x] = 'M'
+            playerGrid[enemy_y][enemy_x] = 'X'
             battleship.miss()
             break
         elif _s == 'D':
-            playerGrid[y][x] = 'M'
+            playerGrid[enemy_y][enemy_x] = 'O'
             battleship.defeat()
             break
         else:
